647c67434a6ef7ef967eda84736bffee6a0f7933
UmarDahil-full <nazarbeksavkanov@gmail.com>
2024-09-17 10:41:24 +0600

    Creating database cinema_db & updating settings.py

--- cinema/cinema/settings.py
+++ cinema/cinema/settings.py
@@ -75,8 +75,13 @@ WSGI_APPLICATION = 'cinema.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'cinema_db',
+        'USER': 'cinema_user',
+        'PASSWORD': "qwerty123",
+        'HOST': '127.0.0.1',        # localhost
+        'PORT': '5432'
+
     }
 }
 
@@ -103,9 +108,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Bishkek'
 
 USE_I18N = True
 
