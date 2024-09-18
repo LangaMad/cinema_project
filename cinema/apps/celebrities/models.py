69121afa942d9d83e2715b2c147f1e480c34fdd5
Errorinthekod <ashataskerbekov6@gmail.com>
2024-09-18 10:05:01 +0600

    commit add new model to celebrity

--- cinema/apps/celebrities/models.py
+++ cinema/apps/celebrities/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Celebrity(models.Model):
+    first_name = models.CharField('Имя', max_length=100, auto_now_add = True)
+    surname = models.CharField('Фамилия',max_length=100, auto_now_add=True)
+    birth_date = models.DateTimeField('Дата рождения', auto_now_add = True)
+    biography = models.TextField('Биография', auto_now_add = True)
+    country = models.CharField('Страна', max_length=100)
+    image = models.ImageField('Изображение1', upload_to='post_images/', verbose_name=("Дополнительное изображение"),
+                              blank=True, null=True)
+    is_alive = models.BooleanField('Жив', null=True)
+    short_bio = models.TextField('Биография кратко', null=True, auto_now_add = True)
+
+
