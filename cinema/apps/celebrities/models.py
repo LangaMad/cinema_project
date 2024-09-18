becedc4faf23fc32f52597dbfe4f550397789b70
LangaMAd <artykovh@gmail.com>
2024-09-18 11:44:20 +0600

    update celebrity models do mirate

--- cinema/apps/celebrities/models.py
+++ cinema/apps/celebrities/models.py
@@ -1,6 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Role(models.Model):
+    name = models.CharField('Название', max_length=100, blank = True, null = True)
+    slug = models.SlugField('URL', unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Роль'
+        verbose_name_plural = 'Роли'
+
 class Celebrity(models.Model):
     first_name = models.CharField('Имя', max_length=100, blank = True, null = True)
     surname = models.CharField('Фамилия',max_length=100, blank = True, null = True)
@@ -11,5 +23,15 @@ class Celebrity(models.Model):
                               blank=True, null=True)
     is_alive = models.BooleanField('Жив', blank = True, null = True)
     short_bio = models.TextField('Биография кратко', blank = True, null = True)
+    role = models.ManyToManyField(Role, related_name='celebrities', verbose_name='Роль', blank = True, null = True)
+
+    def __str__(self):
+        return self.first_name + ' ' + self.surname
+
+    class Meta:
+        verbose_name = 'Селебрити'
+        verbose_name_plural = 'Селебрити'
+
+
 
 
