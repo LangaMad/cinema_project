edc48b0a6d0d5519bf0fb610031b45e77da02d4c
Ayuuusha <aisatoktobolotova@gmail.com>
2024-09-19 12:58:22 +0500

    make views*urls for app(film)

--- cinema/apps/film/urls.py
+++ cinema/apps/film/urls.py
@@ -3,6 +3,6 @@ from .views import *
 
 
 urlpatterns = [
-
-
+    path('film_list/', FilmListView.as_view(), name='film_list'),
+    path('film_details/<int:pk>/', FilmDetailView.as_view(), name='film_details'),
 ]
