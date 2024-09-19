23bc5dfff15a75190d39d37c68a23a70a837c60a
Errorinthekod <ashataskerbekov6@gmail.com>
2024-09-19 12:47:08 +0600

    update view celebrity

--- cinema/apps/celebrities/views.py
+++ cinema/apps/celebrities/views.py
@@ -10,7 +10,7 @@ from django.db.models import Q
 
 class CelebrityListView(ListView):
     model = Celebrity
-    template_name = 'celebrities.html'
+    template_name = 'pages/celebrities.html'
     context_object_name = 'celebrities'
     queryset = Celebrity # одно и тоже
     paginate_by = 2
@@ -42,7 +42,7 @@ class CelebrityListView(ListView):
 
 class CelebrityDetailView(DetailView):
     model = Celebrity
-    template_name = 'celebrities.html'
+    template_name = 'pages/celebrities.html'
     context_object_name = 'celebrity'
 
     def get_context_data(self, **kwargs):
