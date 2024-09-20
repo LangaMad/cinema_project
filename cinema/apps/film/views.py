c307080e9f4b91ca266d96c483f8ffd8bae95715
Ayuuusha <aisatoktobolotova@gmail.com>
2024-09-20 12:06:25 +0500

    add trailers&film in html

--- cinema/apps/film/views.py
+++ cinema/apps/film/views.py
@@ -22,12 +22,17 @@ class FilmDetailView(DetailView):
     template_name = 'pages/film_details.html'
     context_object_name = 'film'
     queryset = Film.objects.all()
+    # paginate_by = 1
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['actors'] = Celebrity.objects.filter(role__name="Актер")
         context['directors'] = Celebrity.objects.filter(role__name="Режиссер")
-
+        context['screenwriter'] = Celebrity.objects.filter(role__name="Сценарист")
+        context['producer'] = Celebrity.objects.filter(role__name="Продюссер")
+        context['trailer'] = Trailer.objects.filter(film=self.object)
+        context['frames'] = FilmFrame.objects.filter(film=self.object)
+        context['f'] = Film.objects.all()
         return context
 
 
